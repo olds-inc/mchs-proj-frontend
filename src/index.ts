0673913552bf4567253869d0c7b6a76df869442a
Kirill Statsenko <stoksenatck@gmail.com>
2023-09-17 02:40:45 +0300

    main page placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.scss";
-
-import App from "./components/App/App";
+import App from "./components/App";
 
 createRoot(document.getElementById("root")).render(
   React.createElement(StrictMode, { children: React.createElement(App) })
